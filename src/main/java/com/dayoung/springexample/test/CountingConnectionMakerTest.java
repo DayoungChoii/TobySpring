39cbc407209e41a493b344ebb64d60cf1ef54423
DayoungChoii <young12430@gmail.com>
2021-07-14 00:17:18 +0900

    [토비의 스프링] 1장 스프링의 이해와 원리
     - 125p 까지

---
+++ src/main/java/com/dayoung/springexample/test/CountingConnectionMakerTest.java
@@ -0,0 +1,21 @@
+package com.dayoung.springexample.test;
+
+import com.dayoung.springexample.dao.CountingConnectionMaker;
+import com.dayoung.springexample.dao.CountingDaoFactory;
+import com.dayoung.springexample.dao.UserDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+public class CountingConnectionMakerTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.get("1");
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection conter:" +  ccm.getCounter());
+
+    }
+}
